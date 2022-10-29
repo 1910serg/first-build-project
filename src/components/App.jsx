49387a843c65b494210f93d9cb0b4eef66c4a194
Sergey <1910serg@mail.ru>
2022-10-29 19:28:40 +0300

    Fixed the architecture of the project

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function App(props) {
+  return (
+    <div className="container">
+      <h1 className="container2">
+        Hi there! I'm your first webpack build, Sergo!
+      </h1>
+      <div className="test">Test!</div>
+
+      <hr />
+
+      <div className="icon"></div>
+
+      <hr />
+
+      <div className="scss_test">
+        <h2>I AM THE SCSS</h2>
+      </div>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { App } from './components/App.jsx';
+
+import WebpackIcon from './assets/webpack-icon.svg';
+import MyDate from './MyDate.js';
+
+// --- styles and packages
 import '../public/styles.css';
 import '../public/index.scss';
 
 import './babel';
-import WebpackIcon from './assets/webpack-icon.svg';
-import MyDate from './MyDate.js';
 
 const post = new MyDate('My title!', WebpackIcon);
 console.log(post.toString());
-
-const App = () => (
-  <div className="container">
-    <h1 className="container2">
-      Hi there! I'm your first webpack build, Sergo!
-    </h1>
-    <div className="test">Test!</div>
-
-    <hr />
-
-    <div className="icon"></div>
-
-    <hr />
-
-    <div className="scss_test">
-      <h2>I AM THE SCSS</h2>
-    </div>
-  </div>
-);
 
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement);
